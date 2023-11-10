8118bd93d8bc2bff8727f275cd156af1ba01c555
veprogames <75524847+veprogames@users.noreply.github.com>
2023-11-10 12:34:32 +0100

    Add Dequence Notations & Documentation

--- notations/bignotation.gd
+++ notations/bignotation.gd
@@ -1,14 +1,23 @@
+## Base class for all Notations that format a [BigNumber]
 class_name BigNotation
 extends RefCounted
 
 func _init():
 	pass
 
+## The "Name" Part of the formatted Number (e. g. "Trillion", "aa")
 func get_suffix(_n: BigNumber) -> String:
 	return ""
 
+## The Mantissa Part of the formatted Number (e. g. 123.45)
 func get_number(_n: BigNumber) -> String:
 	return ""
 
+## A human readable name (e. g. "My Notation")
+func get_pretty_name() -> String:
+	return ""
+
+## Return a formatted String from a [BigNumber]
+## Construct the Number from [method get_number] and [method get_suffix]
 func F(n: BigNumber) -> String:
 	return "%s%s" % [get_number(n), get_suffix(n)]
---
+++ notations/sequence/latin_letter_notation.gd
@@ -0,0 +1,6 @@
+## Use the Latin Alphabet as a sequence (~a-zA-Z)
+class_name LatinLetterNotation
+extends BaseSequenceNotation
+
+func _init() -> void:
+	super("~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
